0436051cd006b27253dd6ec8765b1def1be5485c
Mateus Leonardo <m4teusmatos@gmail.com>
2022-11-19 00:52:46 -0300

    chore: changing scheme

--- src/pages/SignIn/schema.ts
+++ src/pages/SignIn/schema.ts
@@ -1,8 +1,5 @@
 import * as yup from "yup";
 
 export const schema = yup.object({
-  password: yup
-    .string()
-    .required("Informe uma senha!")
-    .min(8, "A senha deve conter pelo menos 8 caracteres!"),
+  password: yup.string(),
 });
