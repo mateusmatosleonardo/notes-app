86cf87da2dd4808762bbd9b244239b2e2179527f
Mateus Leonardo <m4teusmatos@gmail.com>
2022-11-26 16:19:21 -0300

    feat: adding router Form in routes

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import { NavigationContainer, useFocusEffect } from "@react-navigation/native"
 import { createStackNavigator } from "@react-navigation/stack";
 import { Home } from "../pages/Home/Home";
-import { Register } from "../pages/Register/Register";
-import { PasswordContext } from "../context/Password/PasswordContext";
+import { Form } from "../pages/Form/Form";
 
 const Stack = createStackNavigator();
 
@@ -16,6 +15,9 @@ export function Routes() {
           name="Home"
           component={Home}
           options={{ gestureEnabled: false }} />
+        <Stack.Screen
+          name="Form"
+          component={Form} />
       </Stack.Navigator>
     </NavigationContainer >
   )
