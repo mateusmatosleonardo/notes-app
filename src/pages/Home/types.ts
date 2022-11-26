ee7b81b891581103c60ba559a03e8f32e94526ae
Mateus Leonardo <m4teusmatos@gmail.com>
2022-11-26 16:18:15 -0300

    feat: creating type of navigation

---
+++ src/pages/Home/types.ts
@@ -0,0 +1,4 @@
+import { NavigationProp } from "@react-navigation/native";
+import { RootStackParamsList } from "../../routes/types";
+
+export type HomeScreenProps = NavigationProp<RootStackParamsList, "Form">;
