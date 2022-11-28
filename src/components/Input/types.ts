50f68718eb6ae0a6bcbf75e63a61db1a33789ff0
Mateus Leonardo <m4teusmatos@gmail.com>
2022-11-27 22:24:17 -0300

    chore: inputprops

--- src/components/Input/types.ts
+++ src/components/Input/types.ts
@@ -16,6 +16,9 @@ export interface InputProps {
   onBlur?:
     | ((e: NativeSyntheticEvent<TextInputFocusEventData>) => void)
     | undefined;
+  onFocus?:
+    | ((e: NativeSyntheticEvent<TextInputFocusEventData>) => void)
+    | undefined;
   style?: StyleProp<TextStyle>;
   secureTextEntry?: boolean | undefined;
   placeholder?: string | undefined;
@@ -23,11 +26,9 @@ export interface InputProps {
   maxLength?: number | undefined;
   keyboardType?: KeyboardTypeOptions | undefined;
   ref?: React.LegacyRef<TextInput> | undefined;
-  onFocus?:
-    | ((e: NativeSyntheticEvent<TextInputFocusEventData>) => void)
-    | undefined;
   onSubmitEditing?:
     | ((e: NativeSyntheticEvent<TextInputSubmitEditingEventData>) => void)
     | undefined;
   returnKeyType?: ReturnKeyTypeOptions | undefined;
+  autoCapitalize?: "none" | "sentences" | "characters" | "words";
 }
