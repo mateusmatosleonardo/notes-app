ceb5ddbc44250eedaec10597a785482746a8d0ba
Mateus Leonardo <m4teusmatos@gmail.com>
2022-12-14 23:00:10 -0300

    feat: creating handleRemove function to remove password

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,22 +8,35 @@ import { useFocusEffect, useNavigation } from '@react-navigation/native';
 import { HomeScreenProps } from './types';
 import IconListEmpty from '../../assets/images/IconListEmpty.png';
 import * as S from './styles';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useAsyncStorage } from '@react-native-async-storage/async-storage';;
 
 export function Home() {
 
   const navigation = useNavigation<HomeScreenProps>();
 
+  const [modalVisible, setModalVisible] = useState(false);
   const [data, setData] = useState<PasswordProps[]>([]);
+
+  const { getItem, setItem } = useAsyncStorage('@savepass:passwords');
 
   async function handleFetchData() {
     try {
-      const response = await AsyncStorage.getItem('@savepass:passwords');
+      const response = await getItem();
       const data = response ? JSON.parse(response) : [];
       setData(data);
     } catch (e) {
       console.log(e);
     }
+  }
+
+  async function handleRemove(id: string | number[] | undefined) {
+    const response = await getItem();
+    const previousData = response ? JSON.parse(response) : [];
+
+    const data = previousData.filter((item: PasswordProps) => item.id !== id);
+
+    setItem(JSON.stringify(data));
+    setData(data);
   }
 
   function ListEmptyComponent() {
@@ -40,7 +53,9 @@ export function Home() {
   }
 
   function renderItem({ item }: ListRenderItemInfo<PasswordProps>) {
-    return <Password {...item} />
+    return <Password {...item}
+      onPress={() => handleRemove(item.id)}
+    />
   }
 
   useFocusEffect(useCallback(() => {
@@ -48,24 +63,26 @@ export function Home() {
   }, []));
 
   return (
-    <S.Container>
-      <Header onPress={() => navigation.navigate('Form')} />
-      {
-        data.length === 0 ?
-          <>
-          </>
-          :
-          <S.WrapperText>
-            <S.TextPassword>Todas as entradas</S.TextPassword>
-          </S.WrapperText>
-      }
-      <FlatList
-        data={data}
-        keyExtractor={(item) => String(item.id)}
-        renderItem={renderItem}
-        ItemSeparatorComponent={SeparatorItem}
-        ListEmptyComponent={() => ListEmptyComponent()}
-      />
-    </S.Container>
+    <React.Fragment>
+      <S.Container>
+        <Header onPress={() => navigation.navigate('Form')} />
+        {
+          data.length === 0 ?
+            <>
+            </>
+            :
+            <S.WrapperText>
+              <S.TextPassword>Todas as entradas</S.TextPassword>
+            </S.WrapperText>
+        }
+        <FlatList
+          data={data}
+          keyExtractor={(item) => String(item.id)}
+          renderItem={renderItem}
+          ItemSeparatorComponent={SeparatorItem}
+          ListEmptyComponent={() => ListEmptyComponent()}
+        />
+      </S.Container>
+    </React.Fragment>
   );
 }
