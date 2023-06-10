e70bdf7bcdad463319ea4fe1466502e77cc94c1f
mateus <m4teusmatos@gmail.com>
2023-06-09 23:00:43 -0300

    refactor: refactor global header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,28 +1,19 @@
 import React from 'react';
-import * as S from './styles';
-import { HeaderProps } from './types';
-import AddIcon from '@expo/vector-icons/MaterialIcons';
-import UserIcon from '@expo/vector-icons/Feather';
-import Add from '@expo/vector-icons/Ionicons';
+import { WrapperHeader, Wrapper, WrapperTitles, Title, SubTitle, WrapperAvatar, Avatar } from './styles';
+import Me from '../../assets/images/avatar.jpeg';
 
-export function Header({ onPress }: HeaderProps) {
+export function Header() {
   return (
-    <S.Container>
-      <S.Wrapper>
-        <S.WrapperProfile>
-          <S.PhotoProfile>
-            <UserIcon name='user' color='#414040' size={28} />
-          </S.PhotoProfile>
-          <S.WrapperText>
-            <S.Title>Seja bem-vindo</S.Title>
-            <S.SubTitle>Sua segurança é nossa prioridade!</S.SubTitle>
-          </S.WrapperText>
-        </S.WrapperProfile>
-        <S.AddNewPassword onPress={onPress}>
-          <Add name='add-circle' color='#fafafa' size={34} />
-          {/* <AddIcon name='add' color='#fafafa' size={24} /> */}
-        </S.AddNewPassword>
-      </S.Wrapper>
-    </S.Container>
+    <WrapperHeader>
+      <Wrapper>
+        <WrapperTitles>
+          <Title>Notas</Title>
+          <SubTitle>Olá, Mateus! 👋</SubTitle>
+        </WrapperTitles>
+        <WrapperAvatar>
+          <Avatar source={Me} />
+        </WrapperAvatar>
+      </Wrapper>
+    </WrapperHeader>
   )
 }
