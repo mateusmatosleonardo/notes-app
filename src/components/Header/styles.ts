b7954c4190c1ebc903e03b966d0da4e1692ae9ef
mateus <m4teusmatos@gmail.com>
2023-06-09 23:01:12 -0300

    style: refactor style global header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,45 +1,39 @@
 import styled from "styled-components/native";
 import { RFValue } from "react-native-responsive-fontsize";
 
-export const Container = styled.SafeAreaView`
-  padding-top: 20px;
-`;
+export const WrapperHeader = styled.SafeAreaView``;
 
 export const Wrapper = styled.View`
   flex-direction: row;
   justify-content: space-between;
-  align-items: center;
-  padding: 30px 20px;
-  background-color: ${({ theme }) => theme.colors.BLUE_SECONDARY};
+  align-items: flex-end;
+  padding: 20px;
+  background-color: ${({ theme }) => theme.colors.primary.WHITE};
 `;
 
-export const WrapperProfile = styled.View`
-  flex-direction: row;
-  align-items: center;
+export const WrapperAvatar = styled.View`
+  width: 62px;
+  height: 62px;
 `;
 
-export const PhotoProfile = styled.View`
-  justify-content: center;
-  align-items: center;
-  padding: 12px;
-  border-radius: 4px;
-  background-color: ${({ theme }) => theme.colors.WHITE};
+export const Avatar = styled.Image`
+  width: 62px;
+  height: 62px;
+  border-radius: 31px;
 `;
 
-export const WrapperText = styled.View`
+export const WrapperTitles = styled.View`
   margin-left: 12px;
 `;
 
 export const Title = styled.Text`
-  font-family: ${({ theme }) => theme.fonts.Nunito_Bold};
-  color: ${({ theme }) => theme.colors.WHITE};
-  font-size: ${({ theme }) => RFValue(theme.sizes.LARGE)}px;
+  font-family: ${({ theme }) => theme.fonts.Inter_SemiBold};
+  color: ${({ theme }) => theme.colors.primary.BLACK};
+  font-size: ${({ theme }) => RFValue(theme.sizes.ULTRA_LARGE)}px;
 `;
 
 export const SubTitle = styled.Text`
-  font-family: ${({ theme }) => theme.fonts.Nunito_Regular};
-  color: ${({ theme }) => theme.colors.WHITE};
-  font-size: ${({ theme }) => RFValue(theme.sizes.SMALL)}px;
+  font-family: ${({ theme }) => theme.fonts.Inter_Medium};
+  color: ${({ theme }) => theme.colors.primary.MIDDLE_GREY};
+  font-size: ${({ theme }) => RFValue(theme.sizes.NORMAL)}px;
 `;
-
-export const AddNewPassword = styled.Pressable``;
