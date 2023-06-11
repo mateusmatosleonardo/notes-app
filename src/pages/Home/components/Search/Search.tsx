b1eec7230e5d04a2f8b4b9ed4916787220edf6e1
mateus <m4teusmatos@gmail.com>
2023-06-10 21:58:01 -0300

    refactor: refactoring search

--- src/pages/Home/components/Search/Search.tsx
+++ src/pages/Home/components/Search/Search.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { PlaceholderSearch, WrapperSearch } from './styles';
-import Icon from '@expo/vector-icons/Fontisto';
+import { PlaceholderSearch, Search as Container } from './styles';
 import { useTheme } from 'styled-components';
 import { SearchProps } from './types';
+import Icon from '@expo/vector-icons/Fontisto';
 
 export function Search({ navigation }: SearchProps) {
 
   const { colors } = useTheme();
 
   return (
-    <WrapperSearch onPress={navigation}>
+    <Container onPress={navigation}>
       <PlaceholderSearch>Pesquise suas notas</PlaceholderSearch>
       <Icon name="search" size={17} color={colors.primary.DARK_GREY} />
-    </WrapperSearch>
+    </Container>
   )
 }
--- src/pages/Home/components/Search/styles.ts
+++ src/pages/Home/components/Search/styles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components/native";
 import { RFValue } from "react-native-responsive-fontsize";
 
-export const WrapperSearch = styled.Pressable`
+export const Search = styled.Pressable`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
