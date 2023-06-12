cc0424ba6d82c7eaec8da7a0584999b365f0f4b7
mateus <m4teusmatos@gmail.com>
2023-06-12 19:53:10 -0300

    chore: rename props the useform hook

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -2,18 +2,15 @@ import { useState } from "react";
 
 export const useForm = () => {
   const [isFocused, setIsFocused] = useState<
-    "service" | "user" | "password" | true | false | null
+    "title" | "content" | true | false | null
   >(null);
 
   return {
-    isFocusedService: isFocused === "service",
-    isFocusedUser: isFocused === "user",
-    isFocusedPassword: isFocused === "password",
-    handleInputFocusService: () => setIsFocused("service"),
-    handleInputBlurService: () => setIsFocused(false),
-    handleInputFocusUser: () => setIsFocused("user"),
-    handleInputBlurUser: () => setIsFocused(false),
-    handleInputFocusPassword: () => setIsFocused("password"),
-    handleInputBlurPassword: () => setIsFocused(false),
+    isFocusedTitle: isFocused === "title",
+    isFocusedContent: isFocused === "content",
+    handleInputFocusTitle: () => setIsFocused("title"),
+    handleInputBlurTitle: () => setIsFocused(false),
+    handleInputFocusContent: () => setIsFocused("content"),
+    handleInputBlurContent: () => setIsFocused(false),
   };
 };
