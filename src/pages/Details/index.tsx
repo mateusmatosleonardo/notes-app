2d0c62eef0ca9b4a79b0e94c493cb65d0c11bc73
mateus <m4teusmatos@gmail.com>
2023-06-12 19:58:04 -0300

    feat: implementing new states and new use details function

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import { Container, ScrollView, Title, Content } from './styleConfig';
 import { useFocusEffect } from '@react-navigation/native';
 import { useDetailsController } from './useDetailsController';
+import { Loading } from '../../components/Loading/Loading';
+import { Popup } from './Popup';
 import HeaderOptions from './HeaderOptions';
 
 export function Details() {
 
-  const { note, handleFetchData, navigation } = useDetailsController();
+  const { note,
+    handleFetchData,
+    handleDelete,
+    navigation,
+    showPopup,
+    setShowPopup,
+  } = useDetailsController();
 
   useFocusEffect(() => {
     handleFetchData();
@@ -19,16 +27,19 @@ export function Details() {
           <HeaderOptions
             onBackPress={() => navigation.goBack()}
             onEditPress={() => console.log('edit')}
-            onDeletePress={() => console.log('delete')}
+            onDeletePress={() => setShowPopup(true)}
           />
           <ScrollView contentContainerStyle={{ paddingHorizontal: 20 }}>
             <Title>{note.note.title}</Title>
             <Content>{note.note.content}</Content>
           </ScrollView>
         </React.Fragment>
-      ) : (
-        <Title>dads</Title>
-      )}
+      ) : (<Loading />)}
+      <Popup
+        showPopup={showPopup}
+        handleCancel={() => setShowPopup(false)}
+        handleDelete={() => handleDelete(note?.note.id)}
+      />
     </Container>
   );
 }
