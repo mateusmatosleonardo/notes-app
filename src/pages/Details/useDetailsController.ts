c37a3cc886580383ace2974b9f1484c64f2961f1
mateus <m4teusmatos@gmail.com>
2023-06-12 19:57:21 -0300

    feat: creating new states and functions for use details

--- src/pages/Details/useDetailsController.ts
+++ src/pages/Details/useDetailsController.ts
@@ -3,30 +3,51 @@ import { useAsyncStorage } from "@react-native-async-storage/async-storage";
 import { CardProps } from "../../components/Card/types";
 import { RouteProp, useNavigation, useRoute } from "@react-navigation/native";
 import { RootStackParamsList } from "../../routes/types";
-import { NoteState } from "./types";
+import { DetailsScreenProps, NoteState } from "./types";
 
 export const useDetailsController = () => {
   const [note, setNote] = useState<NoteState>();
+  const [showPopup, setShowPopup] = useState(false);
 
+  const navigation = useNavigation<DetailsScreenProps>();
   const route = useRoute<RouteProp<RootStackParamsList, "Details">>();
   const { id } = route.params;
 
-  const navigation = useNavigation();
-
-  const { getItem } = useAsyncStorage("@savepass:passwords");
+  const { getItem, setItem } = useAsyncStorage("@savepass:passwords");
 
   const handleFetchData = useCallback(async () => {
     try {
       const response = await getItem();
       const data = response ? JSON.parse(response) : [];
       const foundNote = data.find((item: CardProps) => item.id === id) || null;
-      setTimeout(() => {
-        setNote({ note: foundNote });
-      }, 2000);
+      setNote({ note: foundNote });
     } catch (e) {
       console.log(e);
     }
   }, [id, getItem]);
 
-  return { note, handleFetchData, navigation };
+  async function handleDelete(id: string | number[] | undefined) {
+    try {
+      const response = await getItem();
+      const previousData = response ? JSON.parse(response) : [];
+      const foundNote = previousData.filter(
+        (item: CardProps) => item.id !== id
+      );
+      setShowPopup(false);
+      setItem(JSON.stringify(foundNote));
+      setNote(foundNote);
+      navigation.navigate("Home");
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  return {
+    note,
+    handleFetchData,
+    navigation,
+    showPopup,
+    setShowPopup,
+    handleDelete,
+  };
 };
