1bd14add326e342b06e8ad36da668852ec8e76fc
mateus <m4teusmatos@gmail.com>
2023-06-12 19:59:35 -0300

    refactor: changing form schema

--- src/pages/Form/schema.ts
+++ src/pages/Form/schema.ts
@@ -1,10 +1,6 @@
 import * as yup from "yup";
 
 export const schema = yup.object({
-  servicename: yup.string().required("Informe o nome do serviço!"),
-  username: yup.string().required("Informe seu nome de usuário ou e-mail!"),
-  password: yup
-    .string()
-    .required("Informe uma senha!")
-    .min(6, "A senha deve conter pelo menos 6 caracteres!"),
+  title: yup.string().required("Informe um título, por favor!"),
+  content: yup.string().required("Informe o conteúdo da nota, por favor!"),
 });
