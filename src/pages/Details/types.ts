087b542177a5e46d09e96123781e1344859bf21a
mateus <m4teusmatos@gmail.com>
2023-06-12 19:54:10 -0300

    feat: create type navigation screen details

--- src/pages/Details/types.ts
+++ src/pages/Details/types.ts
@@ -1,4 +1,8 @@
+import { NavigationProp } from "@react-navigation/native";
+import { RootStackParamsList } from "../../routes/types";
 import { CardProps } from "../../components/Card/types";
+
+export type DetailsScreenProps = NavigationProp<RootStackParamsList, "Details">;
 
 export interface NoteState {
   note: CardProps;
