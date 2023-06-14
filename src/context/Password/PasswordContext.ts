78d019370b0bfb9afcf4dce2828b2487f8403b3c
mateus <m4teusmatos@gmail.com>
2023-06-13 23:11:35 -0300

    rafactor: removing context unused

--- src/context/Password/PasswordContext.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-import { PasswordContextProps } from "./types";
-
-export const PasswordContext = createContext<PasswordContextProps>(
-  {} as PasswordContextProps
-);
