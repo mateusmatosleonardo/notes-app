c4aa9f95d6d7f117cdda84d13c3419231ea1fea0
mateus <m4teusmatos@gmail.com>
2023-06-15 23:23:56 -0300

    refactor: refactor type name

--- src/pages/Details/types.ts
+++ src/pages/Details/types.ts
@@ -2,7 +2,10 @@ import { NavigationProp } from "@react-navigation/native";
 import { RootStackParamsList } from "../../routes/types";
 import { CardProps } from "../../components/Card/types";
 
-export type DetailsScreenProps = NavigationProp<RootStackParamsList, "Details">;
+export type DetailsScreenProps = NavigationProp<
+  RootStackParamsList,
+  "DetailsScreen"
+>;
 
 export interface NoteState {
   note: CardProps;
