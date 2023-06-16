c5334adbc4c1422f1a4ee1e601c94e38cea8e0f3
mateus <m4teusmatos@gmail.com>
2023-06-16 20:16:09 -0300

    feat: creating and implementing function to fetch user data and refactoring style component names

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,45 @@
-import React from 'react';
-import { WrapperHeader, Wrapper, WrapperTitles, Title, SubTitle, WrapperAvatar, Avatar } from './styles';
-import Me from '../../assets/images/avatar.jpeg';
+import React, { useEffect, useState } from 'react';
+import { HeaderWrapper, HeaderContainer, HeaderTitles, HeaderTitle, HeaderSubtitle, AvatarWrapper, Avatar } from './styles';
+import { useAsyncStorage } from '@react-native-async-storage/async-storage';
+import { useTheme } from 'styled-components';
+import UserIcon from "@expo/vector-icons/FontAwesome5";
 
 export function Header() {
+
+  const [userData, setUserData] = useState<{ avatar: string, name: string }>();
+
+  const { getItem } = useAsyncStorage('@userData');
+
+  const { colors } = useTheme();
+
+  const getStoredUserData = async (): Promise<void> => {
+    try {
+      const storedData: string | null = await getItem();
+      if (storedData !== null) {
+        setUserData(JSON.parse(storedData));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getStoredUserData();
+  }, []);
+
   return (
-    <WrapperHeader>
-      <Wrapper>
-        <WrapperTitles>
-          <Title>Notas</Title>
-          <SubTitle>Olá, Mateus! 👋</SubTitle>
-        </WrapperTitles>
-        <WrapperAvatar >
-          <Avatar source={Me} />
-        </WrapperAvatar>
-      </Wrapper>
-    </WrapperHeader>
+    <HeaderWrapper>
+      <HeaderContainer>
+        <HeaderTitles>
+          <HeaderTitle>Notas</HeaderTitle>
+          <HeaderSubtitle>Olá, {userData?.name}! 👋</HeaderSubtitle>
+        </HeaderTitles>
+        <AvatarWrapper>
+          <Avatar
+            source={userData?.avatar ? { uri: userData?.avatar } : <UserIcon name="user-circle" size={200} color={colors.primary.DARK_GREY} />}
+          />
+        </AvatarWrapper>
+      </HeaderContainer>
+    </HeaderWrapper>
   )
 }
