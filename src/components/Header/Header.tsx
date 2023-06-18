f6b07f9d0b137fe24f6cad6ba1012248d4794014
mateus <m4teusmatos@gmail.com>
2023-06-17 21:15:27 -0300

    refactor: pulling store data from zustand

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,42 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { HeaderWrapper, HeaderContainer, HeaderTitles, HeaderTitle, HeaderSubtitle, AvatarWrapper, Avatar } from './styles';
-import { useAsyncStorage } from '@react-native-async-storage/async-storage';
 import { useTheme } from 'styled-components';
+import { useUsersStore } from '../../store/users/users';
 import UserIcon from "@expo/vector-icons/FontAwesome5";
 
 export function Header() {
 
-  const [userData, setUserData] = useState<{ avatar: string, name: string }>();
-
-  const { getItem } = useAsyncStorage('@userData');
+  const { state: { user } } = useUsersStore();
 
   const { colors } = useTheme();
-
-  const getStoredUserData = async (): Promise<void> => {
-    try {
-      const storedData: string | null = await getItem();
-      if (storedData !== null) {
-        setUserData(JSON.parse(storedData));
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    getStoredUserData();
-  }, []);
 
   return (
     <HeaderWrapper>
       <HeaderContainer>
         <HeaderTitles>
           <HeaderTitle>Notas</HeaderTitle>
-          <HeaderSubtitle>Olá, {userData?.name}! 👋</HeaderSubtitle>
+          <HeaderSubtitle>Olá, {user?.name}! 👋</HeaderSubtitle>
         </HeaderTitles>
         <AvatarWrapper>
           <Avatar
-            source={userData?.avatar ? { uri: userData?.avatar } : <UserIcon name="user-circle" size={200} color={colors.primary.DARK_GREY} />}
+            source={user?.avatar_url ? { uri: user?.avatar_url } : <UserIcon name="user-circle" size={200} color={colors.primary.DARK_GREY} />}
           />
         </AvatarWrapper>
       </HeaderContainer>
