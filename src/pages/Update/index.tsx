83faff9d32c84eace314f5b0b72e950a17827158
mateus <m4teusmatos@gmail.com>
2023-06-22 23:14:23 -0300

    completely finalizing the update screen

--- src/pages/Update/index.tsx
+++ src/pages/Update/index.tsx
@@ -1,14 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import * as S from './styleConfig';
+import * as Style from './styleConfig';
 import { Keyboard, Platform } from 'react-native';
 import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
-import { useForm as useFormHook } from '../../hooks/useForm';
 import { Controller, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { Input } from '../../components/Input/Input';
 import { Button } from '../../components/Button/Button';
 import { CardProps } from '../../components/Card/types';
-import { flashMessage } from '../../utils/FlashMessage';
 import { useAsyncStorage } from '@react-native-async-storage/async-storage';
 import { Picker } from '@react-native-picker/picker';
 import { useTheme } from 'styled-components';
@@ -21,28 +19,30 @@ import ArrowLeft from '@expo/vector-icons/AntDesign';
 export function UpdateScreen() {
 
   const [categories] = useState<string[]>
-    (['universidade',
+    (['aleatório',
+      'universidade',
       'pesquisa',
       'outro',
       'ideia',
       'podcast',
       'leitura',
       'artigo',
-      'inspiração']);
+      'inspiração',
+      'tarefas',
+      'compras',
+      'finanças',
+      'receitas',
+      'metas',
+      'dicas',
+      'trabalho',
+      'lembrete'
+    ]);
 
   const [selectedCategory, setSelectedCategory] = useState<string>(categories[0]);
 
   const { colors } = useTheme();
 
-  const { isFocusedTitle,
-    handleInputFocusTitle,
-    handleInputBlurTitle,
-    isFocusedContent,
-    handleInputFocusContent,
-    handleInputBlurContent
-  } = useFormHook();
-
-  const { getItem, setItem } = useAsyncStorage('@savepass:passwords');
+  const { getItem, setItem } = useAsyncStorage('@savenote:notes');
 
   const navigation = useNavigation<UpdateScreenProps>();
   const route = useRoute<RouteProp<RootStackParamsList, "UpdateScreen">>();
@@ -75,19 +75,8 @@ export function UpdateScreen() {
 
       await setItem(JSON.stringify(updatedData));
 
-      flashMessage({
-        message: 'Sucesso',
-        description: 'Nota modificada!',
-        type: 'success'
-      });
-
       navigation.navigate("HomeScreen");
     } catch (e: unknown) {
-      flashMessage({
-        message: 'Erro',
-        description: 'Não foi possível modificar a nota.',
-        type: 'danger'
-      });
       console.log(e);
     }
   }
@@ -99,91 +88,80 @@ export function UpdateScreen() {
   }, [note, setValue]);
 
   return (
-    <S.Container behavior={Platform.OS === 'ios' ? "padding" : "height"}>
-      <S.SafeAreaView>
-        <S.Header>
-          <S.Touch onPress={() => navigation.goBack()}>
+    <Style.Container behavior={Platform.OS === 'ios' ? "padding" : "height"}>
+      <Style.SafeAreaView>
+        <Style.Header>
+          <Style.Touch onPress={() => navigation.goBack()}>
             <ArrowLeft name='arrowleft' color={colors.primary.BLACK} size={22} />
-          </S.Touch>
-          <S.Title>Editar nota</S.Title>
-          <S.Invisible />
-        </S.Header>
-        <S.KeyboardDismiss onPress={Keyboard.dismiss}>
-          <S.Form>
+          </Style.Touch>
+          <Style.Invisible />
+        </Style.Header>
+        <Style.KeyboardDismiss onPress={Keyboard.dismiss}>
+          <Style.Form>
             <Controller
               name='title'
               control={control}
               render={({ field: { value, onChange } }) => (
-                <S.InputArea
-                  style={isFocusedTitle ?
-                    { borderColor: colors.primary.DARK_GREY } :
-                    { borderColor: colors.primary.MIDDLE_GREY }
-                  }
-                >
+                <Style.InputArea>
                   <Input
                     value={value}
                     onChangeText={onChange}
                     placeholder='Título'
                     placeholderTextColor={colors.primary.BLACK}
-                    onFocus={handleInputFocusTitle}
-                    onBlur={handleInputBlurTitle}
-                    style={S.styles.input}
+                    style={Style.styles.input}
                   />
-                </S.InputArea>
+                </Style.InputArea>
               )}
             />
             {errors.title &&
-              <S.TextError>
+              <Style.TextError>
                 {errors.title?.message}
-              </S.TextError>}
+              </Style.TextError>}
             <Controller
               name='content'
               control={control}
               render={({ field: { value, onChange } }) => (
-                <S.InputArea
+                <Style.InputArea
                   style={{
                     flex: 1,
-                    alignItems: 'flex-start',
-                    borderColor: isFocusedContent ? colors.primary.DARK_GREY : colors.primary.MIDDLE_GREY
+                    alignItems: 'flex-start'
                   }}>
                   <Input
                     value={value}
                     onChangeText={onChange}
                     placeholder='Conteúdo'
                     placeholderTextColor={colors.primary.BLACK}
-                    style={[S.styles.input, { fontSize: RFValue(16), fontFamily: "Inter_400Regular", }]}
-                    onFocus={handleInputFocusContent}
-                    onBlur={handleInputBlurContent}
+                    style={[Style.styles.input, { fontSize: RFValue(16), fontFamily: "Inter_400Regular", }]}
                     multiline={true}
                   />
-                </S.InputArea>
+                </Style.InputArea>
               )}
             />
             {errors.content &&
-              <S.TextError>
+              <Style.TextError>
                 {errors.content?.message}
-              </S.TextError>}
-          </S.Form>
-          <S.Footer>
-            <S.WrapperPicker>
+              </Style.TextError>}
+          </Style.Form>
+          <Style.Footer>
+            <Style.WrapperPicker>
               <Picker
                 selectedValue={selectedCategory}
                 onValueChange={(itemValue, itemIndex) => setSelectedCategory(itemValue as string)}
-                style={S.styles.picker}
+                style={Style.styles.picker}
               >
                 {categories.map((category, i) => (
                   <Picker.Item key={i} label={category[0].toUpperCase() + category.slice(1)} value={category} />
                 ))}
               </Picker>
-            </S.WrapperPicker>
+            </Style.WrapperPicker>
             <Button
               title='Editar'
-              style={S.styles.button}
+              style={Style.styles.button}
               onPress={handleSubmit(handleEdit as any)}
             />
-          </S.Footer>
-        </S.KeyboardDismiss>
-      </S.SafeAreaView>
-    </S.Container >
+          </Style.Footer>
+        </Style.KeyboardDismiss>
+      </Style.SafeAreaView>
+    </Style.Container >
   );
 }
